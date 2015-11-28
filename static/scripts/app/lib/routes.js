dbade11db49b40d368904ee0cd5df79c90980b65
Ben Lodge <blodge@hubspot.com>
2015-11-28 18:38:57 -0500

    add basic redux example

---
+++ static/scripts/app/lib/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router';
+import App from '../containers/App'
+
+export default (
+  <Route path='/dashboard' component={App}>
+    
+  </Route>
+);
